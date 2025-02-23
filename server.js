2a47d293a2ac56386b6ec0035557d8a6f4367d52
Estefany Sanabria <estefanysanabrias@gmail.com>
2025-02-22 21:52:25 -0600

    Implementación de CongresistaRoute y CongresistaController

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ require("dotenv").config(); //Cargar variables d entorno
 const express = require("express"); //Crear servidor web
 const cors = require("cors"); //Para permitir solicitudes desde otro dominio
 
+
 const db = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const congresistaRoutes = require("./routes/congresistaRoutes");
 const { errorHandler } = require("./middleware/errorHandler");
+
 
 const app = express(); //Instancia del servidor
 app.use(cors()); //Evitar errores al consumir en React
 app.use(express.json()); //Recibir los datos en JSON
+
 
 db.connect((err) => {
   if (err) {
@@ -18,11 +22,18 @@ db.connect((err) => {
   console.log("Conectado a la base de datos MySQL");
 });
 
+
 // Ruta para ver todos los usuarios
 app.use("/api/users", userRoutes);
 
+
+// Ruta para ver todos los congresistas
+app.use("/api/congresistas", congresistaRoutes);
+
+
 // Middleware para manejo de errores
 app.use(errorHandler);
+
 
 // Iniciar servidor
 const PORT = process.env.PORT || 5000;
