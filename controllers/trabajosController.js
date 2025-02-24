f3617278d862fd762039e88c3eff4faa7c7e2abb
Maicol Monge <maicol.monge@catolica.edu.sv>
2025-02-23 20:33:55 -0600

    Backend del subir trabajos

---
+++ controllers/trabajosController.js
@@ -0,0 +1,39 @@
+const db = require("../config/db");
+
+// Obtener todos los autores
+exports.getAutores = (req, res) => {
+    const sql = `
+        SELECT 
+            a.id_autor,
+            u.nombre,
+            u.apellido
+        FROM 
+            autor a
+        JOIN 
+            usuario u ON a.id_usuario = u.id_usuario
+    `;
+
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+
+        
+        const autores = result.map(autor => ({
+            id: autor.id_autor,
+            nombre: `${autor.nombre} ${autor.apellido}`
+        }));
+
+        res.json(autores);
+    });
+};
+
+// // Verifica si al menos uno de los co-autores es congresista
+// exports.VerificarSiCoautorEsCongresista = (req, res) => {
+
+// };
+
+//Guarda el trabajo en la bd
+exports.SubirTrabajo = (req, res) => {
+
+};
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const userRoutes = require("./routes/userRoutes");
 const congresistaRoutes = require("./routes/congresistaRoutes");
 const administrarComiteRoutes = require("./routes/administrarComiteRoutes");
 const registrarAutorRoutes = require("./routes/registrarAutorRoutes");
+const trabajosRoutes = require("./routes/trabajoRoutes");
 const { errorHandler } = require("./middleware/errorHandler");
 
 
@@ -35,8 +36,13 @@ app.use("/api/congresistas", congresistaRoutes);
 
 // Ruta para el administrar comite
 app.use("/api/users", administrarComiteRoutes);
+
 // Ruta para el autor
 app.use("/api/users", registrarAutorRoutes);
+
+// Ruta para subir trabajos
+app.use("/api/users", trabajosRoutes);
+
 // Middleware para manejo de errores
 app.use(errorHandler);
 
