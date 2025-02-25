c5e0606b4f23e6f4bec9abff364a6e9d228ecea1
Angel Magana <maganaangel342@gmail.com>
2025-02-25 10:54:53 -0600

    Obtener información del usuario para mostrarla en home después de registrarse como congresista

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,14 @@ const {
   infoUser,
   registerUser,
   loginUser,
+  homeUserInfo,
 } = require("../controllers/userController");
 
 const router = express.Router(); // Crear un enrutador y para definir las rutas
 
 // Rutas de usuario
 router.get("/datos", infoUser);
+router.get("/infoUser/:id", homeUserInfo);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 module.exports = router;
